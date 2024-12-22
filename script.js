ddd8f624839681f58514b649c7a7b9e9ed03bdcc
sujal-17-nagpal <sujaln0609y@gmail.com>
2024-12-22 13:54:54 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('snake-game').addEventListener('click',()=>{
+    window.location.href = '/snake game/index.html';
+})
+
+document.getElementById('bubble-game').addEventListener('click',()=>{
+    window.location.href = 'bubble.html';
+})
+
+document.getElementById('drago-game').addEventListener('click',()=>{
+    window.location.href = '/drago game/index.html';
+})
+
+document.getElementById('stone-paper-scissor-game').addEventListener('click',()=>{
+    window.location.href = '/stone paper scissor/index.html';
+})
+
+document.getElementById('tic-tac-toe-game').addEventListener('click',()=>{
+    window.location.href = '/tic tac toe/index.html';
+})
